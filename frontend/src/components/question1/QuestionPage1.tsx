59d1071c3435c41bb49c195368085db4d412a713
bbntsc <bibiana.tosic@edu.fh-wien.ac.at>
2025-01-12 20:04:43 +0100

    necessary imports for question page 1

--- frontend/src/components/question1/QuestionPage1.tsx
+++ frontend/src/components/question1/QuestionPage1.tsx
@@ -1,4 +1,17 @@
+
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import SelfImprovementIcon from "@mui/icons-material/SelfImprovement";
+import HomeIcon from "@mui/icons-material/Home";
+import SpaIcon from "@mui/icons-material/Spa";
+import { useSelection } from "../../context/SelectionContext";
 import "./QuestionPage1.css";
+
+
+
 
 const QuestionPage1 = () => {
   return (
