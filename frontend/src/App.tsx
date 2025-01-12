3f99b23af35de6df3f732093dbc66aaf783ae47c
Tekla Schrempf <tekla.schrempf@concircle.com>
2025-01-12 17:43:48 +0100

    add landing page with header/footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,15 @@ import QuestionPage1 from "./components/question1/QuestionPage1";
 import QuestionPage2 from "./components/question2/QuestionPage2";
 import RecommendationPage from "./components/recommendations/RecommendationPage";
 import { Feedback } from "@mui/icons-material";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <main>
+      <main style={{ minHeight: "100vh", minWidth: "100vw" }}>
+        {/* Header */}
+        <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/question1" element={<QuestionPage1 />} />
@@ -17,6 +21,8 @@ function App() {
           <Route path="/recommendation" element={<RecommendationPage />} />
           <Route path="/feedback" element={<Feedback />} />
         </Routes>
+        {/* Footer */}
+        <Footer />
       </main>
     </Router>
   );
