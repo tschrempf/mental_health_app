ae380334b8b46eddcf6d3c1c2989cd3e8559bab1
Tekla Schrempf <tekla.schrempf@concircle.com>
2025-01-12 17:05:27 +0100

    add feedback to structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from "./components/landingPage/LandingPage";
 import QuestionPage1 from "./components/question1/QuestionPage1";
 import QuestionPage2 from "./components/question2/QuestionPage2";
 import RecommendationPage from "./components/recommendations/RecommendationPage";
+import { Feedback } from "@mui/icons-material";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/question1" element={<QuestionPage1 />} />
           <Route path="/question2" element={<QuestionPage2 />} />
           <Route path="/recommendation" element={<RecommendationPage />} />
+          <Route path="/feedback" element={<Feedback />} />
         </Routes>
       </main>
     </Router>
---
+++ frontend/src/components/feedback/Feedback.tsx
@@ -0,0 +1,11 @@
+import "./Feedback.css";
+
+const FeebackPage = () => {
+  return (
+    <div>
+      <h1>Feedback Page!</h1>
+    </div>
+  );
+};
+
+export default FeebackPage;
